e0d68f251250d1dafce925e68af218f5eba4626f
AyumiizZ <Korrawitc@gmail.com>
2019-01-14 10:51:20 +0700

    :tada: init template

---
+++ junior/Computer System Lab/lab0.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int bitAnd(int x, int y){
+
+}
+
+int getByte(int x,int n){
+
+}
+
+int logicalShift(int x, int n){
+
+}
+
+int binCount(int x){
+
+}
+
+int odd_ones(unsigned x){
+
+}
+
+int tmin(){
+
+}
+
+int fitBits(int x,int n){
+
+}
+
+int negate(int x){
+
+}
+
+int isPositive(int x){
+
+}
+
+int isLessOrEqual(int x,int y){
+
+}
+
+int main(){
+
+}
