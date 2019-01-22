fafc332e13391c08562265c3f4798df1c18f04d3
AyumiizZ <Korrawitc@gmail.com>
2019-01-22 22:30:11 +0700

    :fire: remove old testcase

--- junior/Computer System Lab/lab0.c
+++ junior/Computer System Lab/lab0.c
@@ -57,7 +57,7 @@ void print(int predict,int ans){
     }
 }
 
-void printh(int predict,int ans){
+void printhex(int predict,int ans){
     if(predict == ans){
         printf("CORRECT: Ans is 0x%x\n",ans);
     }
@@ -67,18 +67,5 @@ void printh(int predict,int ans){
 }
 
 int main(){
-    print(4,bitAnd(6,5));
-    print(0x56,getByte(0x12345678,1));
-    printh(0x08765432,logicalShift(0x87654321,4));
-    print(2,binCount(5));
-    print(3,binCount(7));
-    print(1,odd_ones(4));
-    print(0,odd_ones(0xff000000));
-    print(-2147483648,tmin());
-    print(0,fitBits(5,3));
-    print(1,fitBits(-4,3));
-    print(-1,negate(1));
-    print(0,isPositive(-1));
-    print(1,isLessOrEqual(4,5));
     return 0;
 }
