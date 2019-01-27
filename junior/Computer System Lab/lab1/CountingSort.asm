cd18dea54b0e5811a1ca8e329e95842c90be8708
AyumiizZ <Korrawitc@gmail.com>
2019-01-27 13:30:00 +0700

    :art: make code beautiful

--- junior/Computer System Lab/lab1/CountingSort.asm
+++ junior/Computer System Lab/lab1/CountingSort.asm
@@ -12,77 +12,87 @@
 
 main:
     lw $s0, RANGE($zero) # $s0 = 255
-    # set $t0 = i
-initLoop1:
-    add $t0, $zero , $zero # i = 0
-Loop1:
-    slt $t4, $s0, $t0  # if RANGE < i set $t4 = 1 else $t4 = 0
-    bne $t4, $zero, initLoop2 # if RANGE < i goto initLoop2
-    sb	$zero, count($t0)	# store 0 to 
-    addi $t0, $t0, 1 # i = i + 1
-    j Loop1 # jump to Loop1
 
-initLoop2:
+InitLoop1:
     add $t0, $zero , $zero # i = 0
-Loop2:
+ConditionLoop1:
+    slt $t4, $s0, $t0  # if RANGE < i set $t4 = 1 else $t4 = 0
+    bne $t4, $zero, InitLoop2 # if RANGE < i goto InitLoop2
+CodeBlockLoop1:
+    sb	$zero, count($t0)	# store 0 to 
+IncrementLoop1:
+    addi $t0, $t0, 1 # i = i + 1
+    j ConditionLoop1 # jump to ConditionLoop1
+
+InitLoop2:
+    add $t0, $zero , $zero # i = 0
+ConditionLoop2:
     lb $t1, str($t0) # $t1 = str[i]
-    beq $t1, $zero, initLoop3 # if str[i] == 0 goto initLoop3
+    beq $t1, $zero, InitLoop3 # if str[i] == 0 goto InitLoop3
+CodeBlockLoop2:
     lb $t2, count($t1) # $t2 = count[str[i]]
     addi $t2, $t2, 1 # $t2 = 1
     sb $t2, count($t1) # count[str[i]] = $t2
+IncrementLoop2:
     addi $t0, $t0, 1 # i = i + 1
-    j Loop2 # jump to Loop2
+    j ConditionLoop2 # jump to ConditionLoop2
 
-initLoop3:
+InitLoop3:
     addi $t0, $zero , 1 # i = 1
-Loop3:
+ConditionLoop3:
     slt $t4, $s0, $t0  # if RANGE < i set $t4 = 1 else $t4 = 0
-    bne $t4, $zero, initLoop4 # if RANGE < i goto initLoop4
+    bne $t4, $zero, InitLoop4 # if RANGE < i goto InitLoop4
+CodeBlockLoop3:
     addi $t1, $t0, -1 # $t1 = $t0 - 1
     lb $t2, count($t0) # $t2 = count[t0]
     lb $t3, count($t1) # $t3 = count[t1]
     add $t2, $t2, $t3 # count[t0] = count[t0] + count[t1]
     sb $t2, count($t0) # count[t0] = $t2
+IncrementLoop3:
     addi $t0, $t0, 1 # i = i + 1
-    j Loop3 # jump to Loop3
+    j ConditionLoop3 # jump to ConditionLoop3
 
-initLoop4:
+InitLoop4:
     add $t0, $zero , $zero # i = 0
-Loop4:
+ConditionLoop4:
     lb $t1, str($t0) # $t1 = str[i]
-    beq $t1, $zero, initLoop5 # if str[i] == 0 goto initLoop5
+    beq $t1, $zero, InitLoop5 # if str[i] == 0 goto InitLoop5
+CodeBlockLoop4:
     lb $t1, str($t0) # $t1 = set[t0]
     lb $t2, count($t1) # $t2 = count[t1]
     addi $t3, $t2, -1 # $t3 = $t2 - 1
     sb $t1, output($t3) # output[t3] = $t1
     addi $t2, $t2, -1 # $t2 = $t2 - 1
     sb $t2, count($t1) # count[t1] = $t2
+IncrementLoop4:
     addi $t0, $t0, 1 # i = i + 1
-    j Loop4 # jump to Loop4
+    j ConditionLoop4 # jump to ConditionLoop4
 
-initLoop5:
+InitLoop5:
     add $t0, $zero , $zero # i = 0
-Loop5:
+ConditionLoop5:
     lb $t1, str($t0) # $t1 = str[i]
     beq $t1, $zero, printRes # if str[i] == 0 goto printRes
+CodeBlockLoop5:
     lb $t1, output($t0) # $t1 = output[t0]
     sb $t1, str($t0) # str[t0] = t1
+IncrementLoop5:
     addi $t0, $t0, 1 # i = i + 1
-    j Loop5 # jump to Loop5
+    j ConditionLoop5 # jump to ConditionLoop5
 
 printRes:
-    li $v0, 4
-    la $a0, outputstr
+    li $v0, 4 # print string
+    la $a0, outputstr # printf("Sorted string is ");
     syscall
 
-    li $v0, 4
-    la $a0, str
+    li $v0, 4 # print string
+    la $a0, str # printf("%s",str);
     syscall
 
-    li $v0, 4
-    la $a0, endl
+    li $v0, 4 # print string
+    la $a0, endl # printf("\n");
     syscall
 
 end:
-    li $v0, 10
+    li $v0, 10 # return 0
     syscall
